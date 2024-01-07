08b3a9852992cb72971de4df38916beb28791078
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-01-07 02:37:53 -0800

    Hollow_Square.py

--- Hollow_Square.py
+++ Hollow_Square.py
@@ -3,6 +3,6 @@ def hollow_square(rows):
     if i == 0 or i == rows -1:
       print("*" * rows)
     else:
-      print("*" + " " * (row -2) + "*")
+      print("*" + " " * (rows -2) + "*")
 
 hollow_square(5)
